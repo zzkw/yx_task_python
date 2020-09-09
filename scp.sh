8e3d555700cb7fb07d5923a0826d80a24c788188
zzkw <48167061+zzkw@users.noreply.github.com>
2020-09-09 10:40:47 +0800

    scp远程传输文件

--- scp.sh
+++ scp.sh
@@ -4,11 +4,11 @@
 #send "echo $md5s >> /data1/asiainfo/interface/BAS/data/check"
 set user "root"
 set host "192.168.232.129"
-set password "zzkw135"
+set password "***"
 set timeout -1
-set sourceFilePath "/data1/asiainfo/interface/BAS/data/"
+set sourceFilePath "/data1/***"
 set filename [ lindex $argv 0 ]
-set hostFilePath "/data7/asiainfo/interface/temp/houqiang"
+set hostFilePath "/data7/***"
 spawn scp $sourceFilePath$filename check $user@$host:$hostFilePath
 expect "*assword:*"
 send "$password\r"
